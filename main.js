f2a702194044ba38d2c2efbf712684c95406d196
Bruno Richard <bruno683@outlook.fr>
2021-05-25 09:41:12 +0200

    Add JavaScript file

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(() => {
   
 });
+
+const key = document.getElementsByClassName('key');
+const del = document.querySelector('del');
+const result = document.querySelector('result');
+const reset = document.querySelector('reset');
+const output = document.getElementsByClassName('screen');
+const dot = document.querySelector('dot');
+const 
+
+function displayNumber(){
+  key.addEventListenner('click', function (e){
+    value = e.target.value;
+    output.innertext = value;
+      //affiche la valeur de la touche sur l'écran
+  });
+};
